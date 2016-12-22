cf8fcd0bee17e457df56b06e775be9e280b4eb65
William Mc Cormack <wmccormack@hotmail.co.uk>
2016-12-22 19:37:53 +0000

    first commit

---
+++ PrimaryClass.java
@@ -0,0 +1,22 @@
+package wmccormack.guess.the.number;
+
+/**
+ * Guess The Number is a simple game utilising a single class
+ * @author wmccormack
+ *
+ */
+public class PrimaryClass {
+
+	public PrimaryClass() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * Main method will be used to invoke methods
+	 * @param args
+	 */
+	public static void main(String[] args){
+		
+	}
+
+}
